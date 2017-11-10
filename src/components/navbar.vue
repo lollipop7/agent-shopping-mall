eca6a0939b39d5336d6f2422eca7cb91118b7bfe
qgf-guanghuisuiyue <1101801609@qq.com>
2017-11-10 14:32:22 +0800

    排序

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -19,23 +19,11 @@
               休闲食品
             </router-link>
           </MenuItem>
-          <Submenu name="3">
-            <template slot="title">
+          <MenuItem name="3">
               <router-link to="/beautyMakeup" active-class="active">
                 个护美妆
               </router-link>
-            </template>
-            <MenuGroup title="使用">
-              <MenuItem name="3-1">新增和启动</MenuItem>
-              <MenuItem name="3-2">活跃分析</MenuItem>
-              <MenuItem name="3-3">时段分析</MenuItem>
-            </MenuGroup>
-            <MenuGroup title="留存">
-              <MenuItem name="3-4">用户留存</MenuItem>
-              <MenuItem name="3-5">流失用户</MenuItem>
-            </MenuGroup>
-          </Submenu>
-
+          </MenuItem>
           <MenuItem name="4">
             <router-link to="/literatureBook" active-class="active">
               图书文学
